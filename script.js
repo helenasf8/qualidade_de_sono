a318fbc1981640727a5b0489f94101917b14835d
helenasf8 <helenadasilvafortunato.if@gmail.com>
2024-12-01 18:36:03 -0300

    css e teste js

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form');
+const dorme = document.getElementById('horariodedormir');
+const acorda = document.getElementById('horarioacordar')
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+if (horasDeSono >= 8) {
+    document.write(`teste`)
+}
