2da1f82b7fb5da2f85885b29b01ef178137fc7db
helenasf8 <helenadasilvafortunato.if@gmail.com>
2024-12-02 00:40:26 -0300

    correção de erros e uma parte do js

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const form = document.getElementById('form');
-const dorme = document.getElementById('horariodedormir');
-const acorda = document.getElementById('horarioacordar')
-
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 })
+function hora() {
+    let dorme = document.getElementById('horariodedormir');
+    let acorda = document.getElementById('horarioacordar');
+    let horaTela = document.getElementById('horariotelas');
+    let res = document.getElementById('res');
 
-if (horasDeSono >= 8) {
-    document.write(`teste`)
+    let n1 = Number(dorme.value);
+    let n2 = Number(acorda.value);
+
+    if (n1 == 0) {
+        horaDorme = 0;
+    }
+    else {
+        horaDorme = (n1 - 24) * (-1);
+    }
+    let horaAcorda = n2 + horaDorme;
+    
+    if (horaAcorda >= 8) {
+        res.innerHTML = `Você está dormindo ${horaAcorda} horas por noite.`;
+    }
+    else {
+        let horasAdd = 8 - horaAcorda;
+        res.innerHTML = `Você está dormindo ${horaAcorda} horas por noite, deve dormir mais ${horasAdd}H.`;
+
+    }
+    
+    let telaHora = Number(horaTela.value);
+    let horaIdeal = n1 - 1;
+    if (telaHora == n1) {
+        horaDiferenca = telaHora - horaIdeal;
+        tela.innerHTML = `Recomendamos também você a deixar de usar telas ${horaDiferenca}H antes ir dormir.`;
+}
 }
