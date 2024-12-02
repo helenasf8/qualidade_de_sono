6a5f20333b290a60083ebe928e5a44fd837b8715
helenasf8 <helenadasilvafortunato.if@gmail.com>
2024-12-02 08:52:16 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 const form = document.getElementById('form');
-form.addEventListener("submit", (e) => {
+form.addEventListener("click", (e) => {
     e.preventDefault();
-})
-function hora() {
+    
     let dorme = document.getElementById('horariodedormir');
     let acorda = document.getElementById('horarioacordar');
     let horaTela = document.getElementById('horariotelas');
     let res = document.getElementById('res');
 
-    let n1 = Number(dorme.value);
-    let n2 = Number(acorda.value);
 
-    if (n1 == 0) {
-        horaDorme = 0;
-    }
-    else {
-        horaDorme = (n1 - 24) * (-1);
-    }
-    let horaAcorda = n2 + horaDorme;
-    
-    if (horaAcorda >= 8) {
-        res.innerHTML = `Você está dormindo ${horaAcorda} horas por noite.`;
-    }
-    else {
-        let horasAdd = 8 - horaAcorda;
-        res.innerHTML = `Você está dormindo ${horaAcorda} horas por noite, deve dormir mais ${horasAdd}H.`;
+    let tempodorme = dorme.value.split(":");
+    let tempoacorda = acorda.value.split(":");
 
+    let hradorme = Number(tempodorme[0]);
+    let hradormeminuto = Number(tempodorme[1]);
+
+    let hraacorda = Number(tempoacorda[0]);
+    let hraacordaminuto = Number(tempoacorda[1]);
+
+    let horaSono = 0;
+
+    if (hraacorda < hradorme){
+        horaSono = (24-hradorme)+hraacorda
+    }else{
+        horaSono = hraacorda-hradorme
     }
-    
-    let telaHora = Number(horaTela.value);
-    let horaIdeal = n1 - 1;
-    if (telaHora == n1) {
-        horaDiferenca = telaHora - horaIdeal;
-        tela.innerHTML = `Recomendamos também você a deixar de usar telas ${horaDiferenca}H antes ir dormir.`;
-}
-}
+
+    minutoSono = hraacordaminuto - hradormeminuto;
+    if (minutoSono < 0) {
+        minutoSono *= -1;
+    }
+
+    console.log(horaSono+":"+minutoSono);
+
+})
